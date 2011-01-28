02b91bf40e6fe3779f1d332ee4fdcfe86189be33
Julio Daniel Joachin Martinez <Daniel@Navi.local>
2011-01-27 18:46:32 -0600

    lic primer commit

---
+++ lab2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab2::Application.config.secret_token = '9aed108a099bb8d9c667d24a801cbf03d596baf021a718a8447016719b5a7ada90f077b4aa82964df1dbd499dda126c6d5cd4c7dccbb6a36efb2f0985bb6ebae'
